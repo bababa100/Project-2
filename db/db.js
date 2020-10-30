aa0c8cee6cd652c73d5b71ba3f6b4ae6c1352aa1
David_Lane <davidlanemca@gmail.com>
2020-10-30 13:35:10 -0500

    DB connected

---
+++ db/db.js
@@ -0,0 +1,15 @@
+// DATABASE CONFIGURATION
+const mongoose = require('mongoose')
+const db = mongoose.connection
+const mongodbURI = process.env.MONGODBURI || "mongodb://localhost:27017/bababa"
+mongoose.connect(
+    mongodbURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+    },
+)
+db.on('connected', () => console.log('DATABASE Mongoose connected, established at', mongodbURI))
+db.on('error', err => console.log(err.message + ' is mongod not running?'))
+db.on('disconnected', () => console.log('mongo disconnected'))
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
-//require('./db/db')
+require('./db/db.js')
 
 app.get('/', (req, res) =>
     res.send("Ready to Rock"))
 
 
 app.listen(PORT, () => {
-    console.log("Server is Listeneing!!!")
+    console.log("Server is Listening!!!")
 })
