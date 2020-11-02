008cfe723c186929e00bf12a8b9e42abf6adc459
David_Lane <davidlanemca@gmail.com>
2020-11-02 06:17:51 -0600

    Routes back working

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3000
+const express = require('express');
+const app = express();
+const router = require('express').Router();
+const PORT = process.env.PORT || 3000;
+const sellers = express.Router();
 require('./db/db.js')
 
-app.get('/', (req, res) =>
-    res.send("Ready to Rock"))
+
+//Middleware
+app.use(express.static('public'))
+app.use('/css', express.static)
+
+//ROUTES
+//Index of Sellers 
+app.get('/', async (req, res) => {
+
+    res.render('sellers/index.ejs')
+})
+
+//New Sellers Route
+app.get('/new', (req, res) => {
+    res.render('sellers/new.ejs')
+})
+
+
+//Create Seller Route
+router.post('/', (req, res) => {
+    res.send('sellers/new')
+})
 
 
 app.listen(PORT, () => {
